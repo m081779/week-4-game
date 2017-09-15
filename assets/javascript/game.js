1fdac0a627648df977e61044a7c5ddf59ffd39ef
m081779 <marcoprincipio@hotmail.com>
2017-09-15 17:48:47 -0400

    mostly working prototype, still some major pieces missing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
+	$('.heroArea h3').hide();
+	$('.enemyArea h3').hide();
+	$('.remainingOpponents h3').hide();
+	// game.writeHP();
 
-var game = {
+
+
+ var game = {
+
+	
 	
 	"character1": {
 		name: "character1",
 		HP: 100,
 		AP: 10,
 		growAP:10,
-		counterAP: 25
+		counterAP: 100
 	},
 
 	"character2": {
@@ -33,40 +41,72 @@ var game = {
 		growAP:10,
 		counterAP: 25
 	},
-
-
-
+	// id: '',
 	hero: '',
 	enemy: '',
 	clickCounter: 0,
+	winCounter: 0,
+	endState: false,
 
+	startState: function () {
+		location.reload()
+	},
+
+	writeHP: function () {
+		// console.log('writeHP firing')
+		$('.character').each(function (i) {
+		 	 id = $(this).attr('id');
+		 	$(this).find('span').text(game[id].HP);
+		});
+	},
 	getHero: function() {
 		if (game.clickCounter===0) {
 			game.hero = $(this).attr('id');
-			$(this).addClass('hero');
-			var defender = $(this).siblings().detach();
-			defender.appendTo('.defenderSelect');
-			defender.addClass('defender');
+
+			$(this).siblings().addClass('rotate').css({
+				'transform': "rotateY(180deg)"
+			});
+
+			$('.rotate h4').css({
+				'transform': 'rotateY(180deg)'
+			});
+			
+			$('.heroArea h3').show();
+			$('.playerSelect').addClass('defenderSelect');
+			$(this).addClass('hero')
+				   .detach()
+				   .appendTo('.heroArea');
 			game.clickCounter++;
 		}
 	}, //end of getHero method  
 
 	getDefender: function () {
 		if (game.clickCounter===1) {
+			// $(this).css({
+			// 	'transform': "rotateY(180deg)"
+			// });
+			$('.enemyArea h3').show();
+			$('.remainingOpponents h3').show();
 			game.enemy = $(this).attr('id');
-			var remainingOpponents = $(this).siblings().detach();
-			remainingOpponents.appendTo('.remainingOpponents');
+			var defender = $(this).siblings().detach();
+			defender.addClass('defender')
+					.appendTo('.remainingOpponents');
+			// var remainingOpponents = $(this).siblings().detach();
+			$("#"+ game.enemy).appendTo('.enemyArea');
 			game.clickCounter++;
 		}
 	}, //end of getDefender method
 
 	attack: function () {
-		var heroCalc = game[game.hero].HP -= game[game.enemy].AP;
+
+		var heroCalc = game[game.hero].HP -= game[game.enemy].counterAP;
 		var enemyCalc = game[game.enemy].HP -= game[game.hero].AP;
 		game.writeHP(heroCalc, enemyCalc );
 		game[game.hero].AP+=game[game.hero].growAP
-		// console.log(game[game.hero].HP);
-		console.log("Hero's new AP: ", game[game.hero].AP);
+		game.removeEnemy();
+		game.checkLoss();
+		game.checkWin();
+		game.getNewEnemy();
 
 	}, //end of attack method
 
@@ -76,16 +116,70 @@ var game = {
 
 		heroHP.text(input1);
 		enemyHP.text(input2);
+	},
 
+	removeEnemy: function () {
+		var localEnemy = $('#'+game.enemy);
+		if (game[game.enemy].HP<=0) {
+			localEnemy.remove();
+			game.enemy = '';
+			game.clickCounter--;
+			game.winCounter++;
+			console.log("win counter: ", game.winCounter);
+		}
+	},
+
+	getNewEnemy: function () {
+		if (game.clickCounter===1) {
+			$(this).detach().appendTo('.enemyArea');
+			game.enemy = $(this).attr('id');
+			game.clickCounter++;
+		}
+	},
+
+	checkWin: function () {
+
+		if (game.winCounter===3){
+			$('body').off('click', '.character', game.getHero);
+
+		$('body').off('click','.defenderSelect .character', game.getDefender);
+
+		$('body').off('click', '.remainingOpponents .character', game.getNewEnemy)
+		
+		$('body').off('click', 'button', game.attack);
+			game.endState = true;
+			$('.textArea').text('YOU WIN!');
+			setTimeout(game.startState, 4000);
+		}
+	},
+
+	checkLoss: function () {
+		if (game[game.hero].HP<=0) {
+			$('body').off('click', '.character', game.getHero);
+
+		$('body').off('click','.defenderSelect .character', game.getDefender);
+
+		$('body').off('click', '.remainingOpponents .character', game.getNewEnemy)
+		
+		$('body').off('click', 'button', game.attack);
+			game.endState = true;
+			$('.textArea').text('YOU LOSE!');
+			setTimeout(game.startState, 4000);
+		}
 	}
 }//end of game object
 
-	$('body').on('click', '.character', game.getHero);
+		// game.writeHP();
 
-	$('body').on('click','.defenderSelect .defender', game.getDefender);
+	if (!game.endstate) {
+		console.log(game.endState)
+		$('body').on('click', '.character', game.getHero);
 
-	$('body').on('click', 'button', game.attack);
+		$('body').on('click','.defenderSelect .character', game.getDefender);
 
-
+		$('body').on('click', '.remainingOpponents .character', game.getNewEnemy)
+		
+		$('body').on('click', 'button', game.attack);
+	}	
 });//end of document.ready function 
 
