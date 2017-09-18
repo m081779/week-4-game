132cf490571f2104b3f5626bdc1390dc6e7ead4f
m081779 <marcoprincipio@hotmail.com>
2017-09-17 20:39:39 -0400

    many changes to the jQuery, also added jQuery file for working offline

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,108 @@
 $(document).ready(function () {
-
+//game object
  var game = {
-	//character objects lines 6-36
-	"character1": {
+	//character objects lines 6-30
+	//array of pristine character objects
+	charSet: [{
 		name: "character1",
 		HP: 100,
 		AP: 10,
 		growAP:10,
 		counterAP: 10
-	},
-
-	"character2": {
+	}, {
 		name: "character2",
 		HP: 100,
 		AP: 50,
 		growAP:50,
 		counterAP: 25
-	},
-
-	"character3": {
+	}, {
 		name: "character3",
 		HP: 100,
 		AP: 10,
 		growAP:10,
 		counterAP: 25
-	},
-
-	"character4": {
+	}, {
 		name: "character4",
 		HP: 100,
 		AP: 10,
 		growAP:10,
 		counterAP: 25
-	},
+	}],
 
 	//initialize variables
 	hero: '',
 	enemy: '',
 	clickCounter: 0,
 	winCounter: 0,
+	writeWin: 0,
+	writeLoss: 0,
+    //method that plays background music
+	playbackgroundMusic: function () {
+		var snd1  = new Audio();
+		var src1  = document.createElement("source");
+		src1.type = "audio/mpeg";
+		src1.src  = "assets/audio/background-music.mp3";
+		snd1.appendChild(src1);
+
+		snd1.play();
+	},//end of playBackgroundMusic method
+
+	//method that plays punch sound effect
+	playPunchSound: function () {
+		var snd2  = new Audio();
+		var src2  = document.createElement("source");
+		src2.type = "audio/mpeg";
+		src2.src  = "assets/audio/Punch.mp3";
+		snd2.appendChild(src2);
+
+		snd2.play();
+	},//end of playPunchSound method
+
+	//method that plays insect sound effect
+	playInsectSound: function () {
+		var snd3  = new Audio();
+		var src3  = document.createElement("source");
+		src3.type = "audio/mpeg";
+		src3.src  = "assets/audio/Insect-Sound.mp3";
+		snd3.appendChild(src3);
+
+		snd3.play();
+	},//end of playInsectSound method
 
 	//method that reloads the page when game is over
 	startState: function () {
-		location.reload()
+		//creates "disposable" character objects that we can modify
+		//the values of and then rewrite them when startState is called
+		 character1 = JSON.parse(JSON.stringify(game.charSet[0]));
+		 character2 = JSON.parse(JSON.stringify(game.charSet[1]));
+		 character3 = JSON.parse(JSON.stringify(game.charSet[2]));
+		 character4 = JSON.parse(JSON.stringify(game.charSet[3]));
+		 jsonArr = [character1, character2, character3, character4];	
+		 //loops through character objects and resets their start position, 
+		 //resets their display and opacity values, and removes 
+		 //classes added during the game
+		$('.character').each(function (i, val) {
+			$(this).appendTo('.playerSelect').css({
+					'display': 'inline-block',
+					'opacity': '1'
+				 }).removeClass('hero foe defender rotate animateL animateR')
+				   .children()
+				   .removeClass('rotate');;
+		});
+		//removes class that was added to control click event
+		$('.playerSelect').removeClass('defenderSelect');
+		//resets the game space and variables 
+		game.hideHeaders();
+		game.writeInstructions('Choose a bug to begin');
+		game.writeSpansHP();
+		game.hero = '';
+		game.enemy = '';
+		game.clickCounter = 0;
+		game.winCounter = 0;
+		//enables click events
+		$('body').on('click', '.character', game.getHero);
+		$('body').on('click','.defenderSelect .character', game.getDefender);
+		$('body').on('click', '.remainingOpponents .character', game.getNewEnemy)			
 	},//end of startState method
 
 	//method that hides the headers of the different categories until
@@ -55,36 +115,47 @@ $(document).ready(function () {
 
 	//writes text to the #textArea
 	writeInstructions: function (inputText) {
-		$('.textArea').html('<h1>BUGZ</h1><p>Welcome to BUGZ: Fight or Die!</p><p>'+inputText+'</p>');
+		setTimeout(function () {$('.textArea').html('<h1 id="title">BUGZ</h1><p id="subtitle">Welcome to BUGZ: Fight or Die!</p><p id="instructions">' + inputText + '</p>Number of wins: ' + game.writeWin + '<p></p><p>Number of losses: ' + game.writeLoss + '</p>'), 50});
 	},//end of writeInstructions method
 
 	//writes the HP data of the character objects to the screen
 	writeSpansHP: function () {
 		$('.character').each(function (i) {
-		 	 id = $(this).attr('id');
-		 	$(this).find('span').text(game[id].HP);
+		 	 var id = $(this).attr('id');
+		 	$(this).find('span').text(game.charSet[i].HP);
 		});
 	},//end of writeSpansHP method
 
+	selectCharacter: function (type, element) {
+		//when "element" is 'this' from a click event, it saves 
+		//the ID value of the clicked element for 
+		//use associating clicked element with 
+		//corresponding object
+		var id = $(element).attr('id');
+		//loops through array of copies of character objects
+		//to select which is the hero or the enemy depending on "type"
+		for (var i=0; i<jsonArr.length; i++){
+			if (jsonArr[i].name==id) {
+				game[type] = jsonArr[i];
+			}
+		}
+	},//end selectCharacter method
+
 	//method that selects which character you fight as
 	getHero: function() {
-
 		//conditional to ensure hero is only chosen once.
 		if (game.clickCounter===0) {
+			//plays insect sound effect 
+			game.playInsectSound();
 			//writes instructions to #textArea
 			game.writeInstructions('Choose the bug you want to destroy!');
-			//saves the ID value of the clicked element for 
-			//use in calculating attack
-			game.hero = $(this).attr('id');
+			//runs selectCharacter to connect clicked element with corresponding object
+			game.selectCharacter("hero", this);
 			//targets siblings of clicked element and rotates them to enemy
 			//mode
-			$(this).siblings().addClass('rotate').css({
-				'transform': "rotateY(180deg)"
-			});
+			$(this).siblings().addClass('rotate');
 			//re-rotates the h4 elements so text won't be backwards
-			$('.rotate h4').css({
-				'transform': 'rotateY(180deg)'
-			});
+			$('.rotate h4').addClass('rotate');
 			//shows the header for .heroArea
 			$('.heroArea h3').show();
 			//adds .defenderSelect so that click event can differentiate
@@ -97,31 +168,13 @@ $(document).ready(function () {
 			game.clickCounter++;
 		}
 	}, //end of getHero method  
-	playbackgroundMusic: function () {
-		var snd1  = new Audio();
-		var src1  = document.createElement("source");
-		src1.type = "audio/mpeg";
-		src1.src  = "assets/audio/background-music.mp3";
-		snd1.appendChild(src1);
-		console.log('music firing');
-
-		snd1.play();
-	},
-
-	playPunchSound: function () {
-		var snd2  = new Audio();
-		var src2  = document.createElement("source");
-		src2.type = "audio/mpeg";
-		src2.src  = "assets/audio/Punch.mp3";
-		snd2.appendChild(src2);
-		console.log('music firing');
-
-		snd2.play();
-	},
+	
 	//method that selects the current defender
 	getDefender: function () {
 		//conditional ensures only one defender is chosen
 		if (game.clickCounter===1) {
+			//plays insect sound effect 
+			game.playInsectSound();
 			//enables click event for the attack button
 			$('body').on('click', 'button', game.attack);
 			//writes instructions to the #textArea
@@ -129,11 +182,10 @@ $(document).ready(function () {
 			//shows the header for .enemyArea and .remainingOpponents
 			$('.enemyArea h3').show();
 			$('.remainingOpponents h3').show();
-			//saves the ID value of the clicked element for 
-			//use in calculating attack
-			game.enemy = $(this).attr('id');
-			//detaches clicked elements siblings and appends 
-			//them to .remainingOpponents
+			//runs selectCharacter to connect clicked element with corresponding object
+			game.selectCharacter("enemy", this);			
+			//takes remaining character elements not selected 
+			//and appends them to .remainingOpponents
 			$(this).siblings()
 				   .detach()
 				   .appendTo('.remainingOpponents');
@@ -146,20 +198,20 @@ $(document).ready(function () {
 
 	//method that runs most of the game, and calculates damage
 	attack: function () {
-		//generating a random number to access a random message each time attack() is run
+		//generates a random number to access a random message each time attack() is run
 		//and writes it to the screen
 		var random = Math.floor(Math.random()*8);
 		var message = ['Get him!', 'Don\'t stop!','You\'ve got him on the ropes!', 'He\'s no match for you!', 'You\'ve done this before...', 'I wouldn\'t mess with you!', 'You\'re one tough son of a BUG!', 'Service guarantees citizenship.  Would you like to know more?'];
 		game.writeInstructions(message[random]);
-
+		//playes punch sound effect
 		game.playPunchSound();
 		//variables to store values of hero and foe HP after attack
-		var heroCalc = game[game.hero].HP -= game[game.enemy].counterAP;
-		var enemyCalc = game[game.enemy].HP -= game[game.hero].AP;
+		var heroCalc = game.hero.HP -= game.enemy.counterAP;
+		var enemyCalc = game.enemy.HP -= game.hero.AP;
 		//writes the calculated HP value to the screen
-		game.writeHP(heroCalc, enemyCalc );
+		game.writeHP(heroCalc, enemyCalc);
 		//increments the AP of the hero by the original AP value
-		game[game.hero].AP+=game[game.hero].growAP
+		game.hero.AP += game.hero.growAP
 		//runs animation, 
 		game.animate();
 		//removes the enemy from the screen, and checks win/loss conditions
@@ -171,8 +223,8 @@ $(document).ready(function () {
 	//produces the fight animation
 	animate: function () {
 		//removing the CSS animation classes and adding them back
-		//to ensure that animation runs everytime the attack button is
-		//clicked.  Settimeout function to ensure that enough time 
+		//to ensure that animation runs every time the attack button is
+		//clicked.  SetTimeout function to ensure that enough time 
 		//goes by to reset animation
 		$('.hero').removeClass('animateL');
 		$('.foe').removeClass('animateR');
@@ -189,8 +241,8 @@ $(document).ready(function () {
 		//variables that store the captured ID strings of hero and enemy
 		//and convert them to jquery ID selectors targeting those elements
 		//h4.hp elements
-		var enemyHP = $("#" + game.enemy + " .hp");
-		var heroHP = $("#" + game.hero + " .hp");
+		var enemyHP = $("#" + game.enemy.name + " .hp");
+		var heroHP = $("#" + game.hero.name + " .hp");
 		//takes arguments and writes them to the correct elements
 		heroHP.text(input1);
 		enemyHP.text(input2);
@@ -198,18 +250,21 @@ $(document).ready(function () {
 
 	//method that removes enemy from the screen and increments winCounter
 	removeEnemy: function () {
-		//variable that stores the captured ID strings of current enemy
-		//and converts it to jquery ID selector for later use
-		var localEnemy = $('#'+ game.enemy);
+		//variable that takes current enemy's name value and 
+		//converts it to jquery ID selector for later use
+		var localEnemy = $('#'+ game.enemy.name);
 		//conditional that checks the HP value of current enemy
-		if (game[game.enemy].HP<=0) {
+		if (game.enemy.HP<=0) {
 			//if enemy has been defeated, instructions are written to #textArea
 			game.writeInstructions('Choose another victim!');
 			//the element of the defeated enemy is removed after a short delay
-			//to ensure that user has time to process the victory
+			//to ensure that user has time to process the victory, and that element
+			//isn't taking up space in the DOM once new enemy is selected
 			setTimeout(function () {
-				localEnemy.remove();
-			},1000);
+				localEnemy.css({
+					'display': 'none'
+				});
+			},500);
 			localEnemy.fadeTo("slow", 0, function () {});
 			//sets the current enemy to an empty string to stop code from executing on it
 			game.enemy = '';
@@ -228,9 +283,8 @@ $(document).ready(function () {
 		if (game.clickCounter===1) {
 			//detaches the clicked enemy and moves them to .enemyArea
 			$(this).detach().appendTo('.enemyArea');
-			//saves the ID value of the clicked element for 
-			//use in calculating attack
-			game.enemy = $(this).attr('id');
+			//runs selectCharacter to connect clicked element with corresponding object
+			game.selectCharacter("enemy", this);
 			//enables the attack button's click event now that a new enemy has been selected
 			$('body').on('click', 'button', game.attack);
 			//increments clickCounter so that only one enemy is chosen
@@ -249,6 +303,8 @@ $(document).ready(function () {
 			$('body').off('click','.defenderSelect .character', game.getDefender);
 			$('body').off('click', '.remainingOpponents .character', game.getNewEnemy)			
 			$('body').off('click', 'button', game.attack);
+			//increments the number of wins for display
+			game.writeWin++
 			//and resets the game with a 4 second delay
 			setTimeout(game.startState, 4000);
 		}
@@ -257,7 +313,7 @@ $(document).ready(function () {
 	//method that checks loss state conditions
 	checkLoss: function () {
 		//conditional checking if hero has lost all HP
-		if (game[game.hero].HP<=0) {
+		if (game.hero.HP<=0) {
 			//if true, instructions are written to the screen
 			$('.hero').fadeTo("slow", 0, function () {});
 			game.writeInstructions('You\'ve lost, but time is a circle.  Your time will come again!');
@@ -266,26 +322,16 @@ $(document).ready(function () {
 			$('body').off('click','.defenderSelect .character', game.getDefender);
 			$('body').off('click', '.remainingOpponents .character', game.getNewEnemy)			
 			$('body').off('click', 'button', game.attack);
+			//increments the number of losses for display
+			game.writeLoss++
 			//and resets the game with a 4 second delay 
 			setTimeout(game.startState, 4000);
 		}
 	}//end of checkLoss method
 
 }//end of game object
-
-		//on page load, headers are hidden, HP values are written to screen,
-		//and start state instructions are written to #textArea
+		//starts the background music, and initializes the game
 		game.playbackgroundMusic();
-		game.writeSpansHP();
-		game.hideHeaders();
-		game.writeInstructions('Choose a bug to begin');
-
-		//click event handlers for various elements
-		$('body').on('click', '.character', game.getHero);
-
-		$('body').on('click','.defenderSelect .character', game.getDefender);
-
-		$('body').on('click', '.remainingOpponents .character', game.getNewEnemy)
-		
+		game.startState()	
 });//end of document.ready function 
 
