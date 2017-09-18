66900aa6135fa978c4027c161ec94dff1d9d55ed
m081779 <marcoprincipio@hotmail.com>
2017-09-18 02:26:43 -0400

    Working game, no known issues  added all audio files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,28 +5,28 @@ $(document).ready(function () {
 	//array of pristine character objects
 	charSet: [{
 		name: "character1",
-		HP: 100,
+		HP: 90,
 		AP: 10,
 		growAP:10,
-		counterAP: 10
+		counterAP: 16
 	}, {
 		name: "character2",
-		HP: 100,
-		AP: 50,
-		growAP:50,
-		counterAP: 25
+		HP: 135,
+		AP: 6,
+		growAP:6,
+		counterAP: 10
 	}, {
 		name: "character3",
-		HP: 100,
-		AP: 10,
-		growAP:10,
-		counterAP: 25
+		HP: 115,
+		AP: 8,
+		growAP:8,
+		counterAP: 12
 	}, {
 		name: "character4",
-		HP: 100,
-		AP: 10,
-		growAP:10,
-		counterAP: 25
+		HP: 150,
+		AP: 4,
+		growAP:4,
+		counterAP: 18
 	}],
 
 	//initialize variables
@@ -36,6 +36,10 @@ $(document).ready(function () {
 	winCounter: 0,
 	writeWin: 0,
 	writeLoss: 0,
+	heroHP: '',
+	enemyHP: '',
+	counterAttack: true,
+
     //method that plays background music
 	playbackgroundMusic: function () {
 		var snd1  = new Audio();
@@ -69,26 +73,47 @@ $(document).ready(function () {
 		snd3.play();
 	},//end of playInsectSound method
 
+	playWinSound: function () {
+		var snd4  = new Audio();
+		var src4  = document.createElement("source");
+		src4.type = "audio/mpeg";
+		src4.src  = "assets/audio/win-music.mp3";
+		snd4.appendChild(src4);
+
+		snd4.play();
+	},//end of playWinSound method
+
+	playLossSound: function () {
+		var snd5  = new Audio();
+		var src5  = document.createElement("source");
+		src5.type = "audio/mpeg";
+		src5.src  = "assets/audio/loss-music.mp3";
+		snd5.appendChild(src5);
+
+		snd5.play();
+	},//end of playLoseSound method
+
 	//method that reloads the page when game is over
 	startState: function () {
+		document.getElementById('background').play();
 		//creates "disposable" character objects that we can modify
 		//the values of and then rewrite them when startState is called
-		 character1 = JSON.parse(JSON.stringify(game.charSet[0]));
-		 character2 = JSON.parse(JSON.stringify(game.charSet[1]));
-		 character3 = JSON.parse(JSON.stringify(game.charSet[2]));
-		 character4 = JSON.parse(JSON.stringify(game.charSet[3]));
-		 jsonArr = [character1, character2, character3, character4];	
-		 //loops through character objects and resets their start position, 
-		 //resets their display and opacity values, and removes 
-		 //classes added during the game
-		$('.character').each(function (i, val) {
-			$(this).appendTo('.playerSelect').css({
+		character1 = JSON.parse(JSON.stringify(game.charSet[0]));
+		character2 = JSON.parse(JSON.stringify(game.charSet[1]));
+		character3 = JSON.parse(JSON.stringify(game.charSet[2]));
+		character4 = JSON.parse(JSON.stringify(game.charSet[3]));
+		jsonArr = [character1, character2, character3, character4];	
+		//loops through character elements and resets their 
+		//start position, their display and opacity values, 
+		//and removes classes added during the game
+		for (var i=1; i<=4; i++) {
+			$('#character'+ i).appendTo('.playerSelect').css({
 					'display': 'inline-block',
 					'opacity': '1'
 				 }).removeClass('hero foe defender rotate animateL animateR')
 				   .children()
-				   .removeClass('rotate');;
-		});
+				   .removeClass('rotate');
+		}
 		//removes class that was added to control click event
 		$('.playerSelect').removeClass('defenderSelect');
 		//resets the game space and variables 
@@ -135,7 +160,7 @@ $(document).ready(function () {
 		//loops through array of copies of character objects
 		//to select which is the hero or the enemy depending on "type"
 		for (var i=0; i<jsonArr.length; i++){
-			if (jsonArr[i].name==id) {
+			if (jsonArr[i].name===id) {
 				game[type] = jsonArr[i];
 			}
 		}
@@ -196,6 +221,21 @@ $(document).ready(function () {
 		}
 	}, //end of getDefender method
 
+	calculateEnemyHP: function () {
+		enemyHP = game.enemy.HP -= game.hero.AP;
+		game.hero.AP += game.hero.growAP;
+		if (game.enemy.HP<=0 && game.hero.HP > 0) {
+			game.counterAttack = false;
+		}
+
+	},
+
+	calculateHeroHP: function () {
+		if (game.counterAttack) {
+		heroHP = game.hero.HP -= game.enemy.counterAP;
+		}
+	},
+
 	//method that runs most of the game, and calculates damage
 	attack: function () {
 		//generates a random number to access a random message each time attack() is run
@@ -205,15 +245,15 @@ $(document).ready(function () {
 		game.writeInstructions(message[random]);
 		//playes punch sound effect
 		game.playPunchSound();
-		//variables to store values of hero and foe HP after attack
-		var heroCalc = game.hero.HP -= game.enemy.counterAP;
-		var enemyCalc = game.enemy.HP -= game.hero.AP;
-		//writes the calculated HP value to the screen
-		game.writeHP(heroCalc, enemyCalc);
-		//increments the AP of the hero by the original AP value
-		game.hero.AP += game.hero.growAP
 		//runs animation, 
 		game.animate();
+		//variables to store values of hero and foe HP after attack
+		game.calculateEnemyHP();
+		game.calculateHeroHP();
+		//writes the calculated HP value to the screen
+		game.writeHP(heroHP, enemyHP);
+		//increments the AP of the hero by the original AP value
+		console.log('counterAttack',game.counterAttack);	
 		//removes the enemy from the screen, and checks win/loss conditions
 		game.removeEnemy();
 		game.checkLoss();
@@ -268,6 +308,9 @@ $(document).ready(function () {
 			localEnemy.fadeTo("slow", 0, function () {});
 			//sets the current enemy to an empty string to stop code from executing on it
 			game.enemy = '';
+
+			game.counterAttack = true;
+
 			//decrements the clickCounter so that another enemy may be chosen only once
 			game.clickCounter--;
 			//increments the winCounter to enable checkWin() to determine win state
@@ -281,8 +324,9 @@ $(document).ready(function () {
 	getNewEnemy: function () {
 		//uses clickCounter variable to ensure that only one new enemy is chosen
 		if (game.clickCounter===1) {
+			game.playInsectSound();
 			//detaches the clicked enemy and moves them to .enemyArea
-			$(this).detach().appendTo('.enemyArea');
+			$(this).addClass('foe').detach().appendTo('.enemyArea');
 			//runs selectCharacter to connect clicked element with corresponding object
 			game.selectCharacter("enemy", this);
 			//enables the attack button's click event now that a new enemy has been selected
@@ -298,6 +342,9 @@ $(document).ready(function () {
 		if (game.winCounter===3){
 			//if true, it writes instructions to the screen
 			game.writeInstructions('You\'ve won!  You\'re KING of the BUGZ!');
+			//plays win music
+			document.getElementById('background').pause();
+			game.playWinSound();
 			//disables all click events
 			$('body').off('click', '.character', game.getHero);
 			$('body').off('click','.defenderSelect .character', game.getDefender);
@@ -306,17 +353,20 @@ $(document).ready(function () {
 			//increments the number of wins for display
 			game.writeWin++
 			//and resets the game with a 4 second delay
-			setTimeout(game.startState, 4000);
+			setTimeout(game.startState, 3500);
 		}
 	},//end of checkwin method
 
 	//method that checks loss state conditions
 	checkLoss: function () {
 		//conditional checking if hero has lost all HP
-		if (game.hero.HP<=0) {
+		if (game.hero.HP<=0) {	
 			//if true, instructions are written to the screen
 			$('.hero').fadeTo("slow", 0, function () {});
 			game.writeInstructions('You\'ve lost, but time is a circle.  Your time will come again!');
+			//plays loss music
+			document.getElementById('background').pause();
+			game.playLossSound();
 			//disables all click events
 			$('body').off('click', '.character', game.getHero);
 			$('body').off('click','.defenderSelect .character', game.getDefender);
@@ -331,7 +381,8 @@ $(document).ready(function () {
 
 }//end of game object
 		//starts the background music, and initializes the game
-		game.playbackgroundMusic();
+		// game.playbackgroundMusic();
+		document.getElementById('background').play();
 		game.startState()	
 });//end of document.ready function 
 
